1108ce5be29a6c15e47d9476eed15a909f9ce703
Omer Ahmed <omerahmed@Mac.telekom.ip>
2025-08-16 20:07:17 +0200

    Prepare for Heroku deployment

--- shopproject/settings.py
+++ shopproject/settings.py
@@ -78,9 +78,11 @@ WSGI_APPLICATION = 'shopproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        dj_database_url.config(os.environ.get("DATABASE_URL"),conn_max_age=600, ssl_require=True)
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
